6c5aea328c67441d3bbf4e7a333db6257f7dd3b0
zhangxinling2 <2350486903@qq.com>
2023-02-15 07:11:42 +0800

    BindJson

--- web/context.go
+++ web/context.go
@@ -1,8 +1,23 @@
 package web
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+)
 
 type Context struct {
-	Resp http.ResponseWriter
-	Req  *http.Request
+	Resp       http.ResponseWriter
+	Req        *http.Request
+	PathParams map[string]string
+}
+
+func (c *Context) BindJSON(val any) error {
+	if val == nil {
+		return errors.New("web:输入不能为nil")
+	}
+	//bs,_:=io.ReadAll(c.Req.Body)
+	//json.Unmarshal(bs,val)
+	decoder := json.NewDecoder(c.Req.Body)
+	return decoder.Decode(val)
 }
